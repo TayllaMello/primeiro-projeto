3e7da2beaa5fb97f49348773220b659dae674a79
taylla.silva <taylla.silva@useall.com.br>
2023-07-10 18:16:47 -0300

    alteracao

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   projectId: '9gb668',
   e2e: {
+    baseUrl: 'https://alura-fotos.herokuapp.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -9,11 +10,13 @@ module.exports = {
 
 {
   "reporter"; "mochawesome",
-  "reporterOptions"; {
-  "reportDir"; "cypress/report/mochawesome-report",
-  "overwrite"; true,
-  "html"; true,
-  "json"; false,
-  "timestamp"; "mmddyyyy_HHMMss"
-  }    
+    "reporterOptions"; {
+    "reportDir"; "cypress/report/mochawesome-report",
+      "overwrite"; true,
+        "html"; true,
+          "json"; false,
+            "timestamp"; "mmddyyyy_HHMMss"
+  }
+
 }
+
--- cypress/e2e/3-ALURAPIC/alurapic.cy.js
+++ cypress/e2e/3-ALURAPIC/alurapic.cy.js
@@ -2,58 +2,27 @@ describe('Login e registro de usuarios alura pic', () => {
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com')
-
     })
 
-    it('verifica mensagens validacao', () => {
-
-        cy.contains('a', 'Register now').click()
-        cy.contains('button', 'Register').click()
-        cy.contains('ap-vmessage', 'Email is required!').should('be.visible')
-        cy.contains('button', 'Register').click()
+    it('verifica mensagens tela inicial', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible')
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible')
-        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible')
+        cy.get('button[type="submit"]').should('be.visible')
+    });
 
-
-    })
-
-    it('verifica mensagem de email inválido', () => {
-
-        cy.contains('a', 'Register now').click()
-        cy.contains('button', 'Register').click()
-        cy.get('input[formcontrolname="email"]').type('taylla')
-        cy.contains('button', 'Register').click()
-        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
-    })
-
-    it('verifica mensagem de nome de usuário inválido', () => {
-
-        cy.contains('a', 'Register now').click()
-        cy.contains('button', 'Register').click()
-        cy.get('input[formcontrolname="fullName"]').type('t')
-        cy.contains('button', 'Register').click()
-        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible')
-    })
-
-    it('verifica mensagem de nome de usuário inválido', () => {
-
-        cy.contains('a', 'Register now').click()
-        cy.contains('button', 'Register').click()
-        cy.get('input[formcontrolname="userName"]').type('t')
-        cy.contains('button', 'Register').click()
-        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible')
-    })
-
-    it('verifica mensagem de senha com menos de 8 caracteres', () => {
-
-        cy.contains('a', 'Register now').click()
-        cy.contains('button', 'Register').click()
+    it('verifica botao habilitado na tela inicial', () => {
+        cy.get('input[formcontrolname="userName"]').type('teste')
         cy.get('input[formcontrolname="password"]').type('123')
-        cy.contains('button', 'Register').click()
-        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
+        cy.get('button[type="submit"]').should('be.enabled')
     })
 
+    it('verifica nome da aplicação na tela inicial', () => {
+        cy.contains('a', ' ALURAPIC ').should('be.visible')
+    });
 
+    it('verifica menu clicavel tela inicial', () => {
+        cy.get('.navbar-brand > .fa').click()
+        cy.get('.menu-bar > .fa').should('be.visible')
+    });
 
-})
+});
